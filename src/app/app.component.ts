cdcba1a5ae27c1e0a51b04555f11a205d9512761
Chris Darden <chris.a.darden@gmail.com>
2024-03-24 14:40:18 -0500

    added cards, added object to hold test data for component output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CardComponent]
 })
 export class AppComponent {
   title = 'pictureTabScroller';
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: "Snowy Mountain",
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: "Mountain Biking",
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today'
+    },
+  ];
+
 }
